1a6feefb20329eb7784feca9dab15b5e0be957da
Ben Pearey <bpearey@amplience.com>
2020-03-28 17:05:06 +0000

    feat(store): new store for selected keys

--- src/components/ammo-graph-key/AmmoGraphKey.svelte
+++ src/components/ammo-graph-key/AmmoGraphKey.svelte
@@ -1,7 +1,34 @@
 <script>
+  import { get } from "svelte/store";
   import { generateHexColourFromString } from "../../services/colour-mapper.service";
+  import { ammoKeys } from "../../store.js";
 
   export let ammunition;
+
+  let selectedKeys;
+
+  ammoKeys.update(() => {
+    return ammunition.map(ammo => ammo.id);
+  });
+
+  const unsubscribe = ammoKeys.subscribe(keys => {
+    selectedKeys = keys;
+  });
+
+  function handleKeyClick(keyId) {
+    ammoKeys.update(a => {
+      const arr = [...a];
+      const index = arr.indexOf(keyId);
+
+      if (index > -1) {
+        arr.splice(index, 1);
+      } else {
+        arr.push(keyId);
+      }
+
+      return arr;
+    });
+  }
 </script>
 
 <style>
@@ -20,17 +47,23 @@
     display: flex;
     align-content: center;
     justify-content: center;
-    color: #555;
+    color: rgb(85, 85, 85);
+  }
+
+  button {
+    border: 1px solid rgba(170, 170, 170, 0.3);
+    border-radius: 6px;
+    padding: 10px 20px;
+  }
+
+  button.selected {
+    background-color: rgba(170, 170, 170, 0.3);
   }
 
   li svg {
-    height: 20px;
-    width: 20px;
-  }
-
-  li svg circle {
-    fill-opacity: 0.6;
-    stroke: rgba(0, 0, 0, 0.5);
+    height: 12px;
+    width: 12px;
+    opacity: 0.6;
   }
 </style>
 
@@ -38,14 +71,18 @@
   <ul>
     {#each ammunition as { id, label }}
       <li>
-        <svg>
-          <circle
-            cx="10"
-            cy="10"
-            r="5"
-            fill={generateHexColourFromString(id)} />
-        </svg>
-        <span>{label}</span>
+        <button
+          on:click={handleKeyClick(id)}
+          class={selectedKeys.includes(id) ? 'selected' : ''}>
+          <svg>
+            <circle
+              cx="6"
+              cy="6"
+              r="5"
+              fill={generateHexColourFromString(id)} />
+          </svg>
+          <span>{label}</span>
+        </button>
       </li>
     {/each}
   </ul>
